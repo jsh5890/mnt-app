7bdfd2d1de82643396d4d450f4f795879e762ead
jsh5890 <jsh02352@naver.com>
2023-02-19 22:32:44 +0900

    무비앱 시리즈 #4 Landing Page 만들기 (1) ( 따라하며 배우는 리액트 노드 )

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -3,4 +3,6 @@ export const USER_SERVER = '/api/users';
 
 export const API_URL = 'https://api.themoviedb.org/3/'
 
-export const IMAGE_BASE_URI = 'http://image.tmdb.org/t/p/'
+export const API_KEY = '1550c71e024c8c9cc90093bfda5da6eb'
+
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/'
